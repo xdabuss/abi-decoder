41d03f5dc06094082bf23653b00f78471c015310
xdabuss <alex@meetparcel.com>
2022-08-16 19:08:26 -0700

    big token id

--- index.js
+++ index.js
@@ -184,11 +184,12 @@ function _decodeLogs(logs) {
           param.type === "int"
         ) {
           // ensure to remove leading 0x for hex numbers
-          if (typeof decodedP.value === "string" && decodedP.value.startsWith("0x")) {
-            decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
-          } else {
-            decodedP.value = new BN(decodedP.value).toString(10);
-          }
+          // if (typeof decodedP.value === "string" && decodedP.value.startsWith("0x")) {
+          //   decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
+          // } else {
+          //   decodedP.value = new BN(decodedP.value).toString(10);
+          // }
+          decodedP.value = new BN(decodedP.value).toString(10);
 
         }
 
